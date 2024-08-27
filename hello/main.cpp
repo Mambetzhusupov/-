24cd82411113f3f30cf8043789add35f2eb435c8
Mambetzhusupov <150808947+Mambetzhusupov@users.noreply.github.com>
2024-08-27 17:41:37 -0500

    Add files via upload

---
+++ hello/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "my name is Abdumalik Mambetzhusupov" << std::endl;
+    std::cout << "Im a Webster University student" << std::endl;
+    std::cout << "I was born on 31st of December" << std::endl;
+    std::cout << "I like hitting gym" << std::endl;
+    return 0;
+}
